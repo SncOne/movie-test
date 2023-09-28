b3f407cb382f4352e827817ca6f471db18c0c643
SncOne <turkergurel19@gmail.com>
2023-09-28 19:36:37 +0300

    header added

--- movie-test/src/app/layout.jsx
+++ movie-test/src/app/layout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './globals.css'
 import Header from '@/component/Header'
+import Head from './head.jsx'
 const Layout = ({ children }) => {
     return (
         <html lang='en'>
+            <Head />
             <body>
                 <Header />
                 {children}
--- movie-test/src/component/Header.jsx
+++ movie-test/src/component/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import '@/app/globals.css'
+import { BiSearch } from 'react-icons/bi';
+
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className='header'>
+            <div>Rotten Tomatoes</div>
+            <div className='headerSearch'>
+                <input className='searchText' placeholder='Search' type="search" name="" id="" pattern="[A-Za-z0-9]{4,20}" />
+                <BiSearch size={20} />
+            </div>
+        </div>
     )
 }
 
